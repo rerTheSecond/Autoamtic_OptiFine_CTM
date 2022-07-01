56e34331e34de1489da94b8ed21967bd3d81c6ef
rerTheSecond <108492421+rerTheSecond@users.noreply.github.com>
2022-07-01 15:15:38 +0300

    Update main.py
    -Fixed a bug in side3
    -Made the code neater and better for importing
    -renamed some variables

--- main.py
+++ main.py
@@ -1,55 +1,57 @@
 import cv2
 from os import mkdir
+from time import time
 
 
-def draw_outline(name, corner1, corner2, corner3, corner4, side1, side2, side3, side4, frames, outlinelength, texture,
+def draw_outline(corner1, corner2, corner3, corner4, side1, side2, side3, side4, frames, outlinelength, texture,
                  outline):
     image = texture.copy()
     sidelength = len(texture[0])
 
     for n in range(frames):
         if corner1:
-            for width in range(outlinelength):
-                for length in range(outlinelength):
-                    image[width + sidelength * n][length] = outline[width + sidelength * n][length]
+            for x in range(outlinelength):
+                for y in range(outlinelength):
+                    image[y + sidelength * n][x] = outline[y + sidelength * n][x]
         if corner2:
-            for length in range(sidelength - outlinelength, sidelength):
-                for width in range(outlinelength):
-                    image[width + sidelength * n][length] = outline[width + sidelength * n][length]
+            for x in range(sidelength - outlinelength, sidelength):
+                for y in range(outlinelength):
+                    image[y + sidelength * n][x] = outline[y + sidelength * n][x]
         if corner3:
-            for width in range(sidelength - outlinelength, sidelength):
-                for length in range(outlinelength):
-                    image[width + sidelength * n][length] = outline[width + sidelength * n][length]
+            for x in range(outlinelength):
+                for y in range(sidelength - outlinelength, sidelength):
+                    image[y + sidelength * n][x] = outline[y + sidelength * n][x]
         if corner4:
-            for width in range(sidelength - outlinelength, sidelength):
-                for length in range(sidelength - outlinelength, sidelength):
-                    image[width + sidelength * n][length] = outline[width + sidelength * n][length]
+            for x in range(sidelength - outlinelength, sidelength):
+                for y in range(sidelength - outlinelength, sidelength):
+                    image[y + sidelength * n][x] = outline[y + sidelength * n][x]
         if side1:
-            for length in range(outlinelength, sidelength - outlinelength):
-                for width in range(outlinelength):
-                    image[width + sidelength * n][length] = outline[width + sidelength * n][length]
+            for x in range(outlinelength, sidelength - outlinelength):
+                for y in range(outlinelength):
+                    image[y + sidelength * n][x] = outline[y + sidelength * n][x]
         if side2:
-            for length in range(outlinelength):
-                for width in range(outlinelength, sidelength - outlinelength):
-                    image[width + sidelength * n][length] = outline[width + sidelength * n][length]
+            for x in range(outlinelength):
+                for y in range(outlinelength, sidelength - outlinelength):
+                    image[y + sidelength * n][x] = outline[y + sidelength * n][x]
         if side3:
-            for width in range(outlinelength - sidelength, sidelength):
-                for length in range(sidelength - outlinelength, sidelength):
-                    image[width + sidelength * n][length] = outline[width + sidelength * n][length]
+            for x in range(sidelength - outlinelength, sidelength):
+                for y in range(outlinelength, sidelength - outlinelength):
+                    image[y + sidelength * n][x] = outline[y + sidelength * n][x]
         if side4:
-            for length in range(outlinelength, sidelength - outlinelength):
-                for width in range(sidelength - outlinelength, sidelength):
-                    image[width + sidelength * n][length] = outline[width + sidelength * n][length]
-    cv2.imwrite(f"result/{str(name)}.png", image)
+            for x in range(outlinelength, sidelength - outlinelength):
+                for y in range(sidelength - outlinelength, sidelength):
+                    image[y + sidelength * n][x] = outline[y + sidelength * n][x]
+    return image
 
 
 def main():
     t = cv2.imread('texture.png')
     o = cv2.imread('outline.png')
+    animation_frames = len(t) // len(t[0])
     while True:
         try:
             w = int(input('Outline width: '))
-            animation_frames = int(input("Animation frames(if u don't know what this does enter '1'): "))
+            start = time()
             break
         except ValueError:
             print("Please enter an integer!")
@@ -57,25 +59,18 @@ def main():
         try:
             mkdir("result")
         except FileExistsError:
-            print("result folder already exists")
+            ...
         with open("connected_tile.txt", "r") as f:
             lines = f.readlines()
             for i in range(47):
-                c1 = bool(int(lines[i][0]))
-                c2 = bool(int(lines[i][1]))
-                c3 = bool(int(lines[i][2]))
-                c4 = bool(int(lines[i][3]))
-                s1 = bool(int(lines[i][4]))
-                s2 = bool(int(lines[i][5]))
-                s3 = bool(int(lines[i][6]))
-                s4 = bool(int(lines[i][7]))
-                draw_outline(i, c1, c2, c3, c4, s1, s2, s3, s4, animation_frames, w, t, o)
-        input("Textures succesefully created, Press ENTER to leave.")
+                result = draw_outline(bool(int(lines[i][0])), bool(int(lines[i][1])), bool(int(lines[i][2])),
+                                      bool(int(lines[i][3])), bool(int(lines[i][4])), bool(int(lines[i][5])),
+                                      bool(int(lines[i][6])), bool(int(lines[i][7])), animation_frames, w, t, o)
+                cv2.imwrite(f"result/{str(i)}.png", result)
+        input(f"Finished in {int(100*(time() - start))} milliseconds. Press ENTER to leave.")
     else:
-        input('Width of texture.png and outline.png does not match.\nPress ENTER to '
-              'leave')
+        input('Width of texture.png and outline.png does not match.\nPress ENTER to leave')
 
 
 if __name__ == '__main__':
     main()
-
